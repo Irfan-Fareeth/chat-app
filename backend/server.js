48583e37c9e4c2deff50c18769c439e0a25b4020
Irfan Fareeth <irfanfareeths@gmail.com>
2025-05-27 23:41:38 +0530

    hi

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ const server = app.listen(PORT, console.log(`Server listening on port ${PORT}`))
 const io = require('socket.io')(server,
     {   pingTimeout: 60000,
         cors: {
-            origin: "https://chatapplicaton.onrender.com"
+            origin: "*"
 
         }
     }
