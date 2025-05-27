59a35a2f60ad50dfa45bdabd0e18977b05ee3bd7
Irfan Fareeth <irfanfareeths@gmail.com>
2025-05-27 20:48:32 +0530

    fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,13 @@
 const mongoose = require("mongoose");
-const connectDB = async()=>{
-    try
-    {
-        console.log(process.env.MONGO_URL);
-        const conn = await mongoose.connect("mongodb+srv://learnerProtocol:Irfan4002@cluster0.qz3pe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{
-        });
-        console.log(`Mongo DB Connected: ${conn.connection.host}`);
-    }
-    catch(error)
-    {
-        console.log(`Error:${error}`);
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL); // use env
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
         process.exit();
     }
-
-}
+};
 
 module.exports = connectDB;
